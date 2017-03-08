c0aef9392ac1da7844cb6170d69e15450f780b92
Richard diZerega <richdizz@outlook.com>
2017-03-08 08:38:33 -0600

    Initial checkin

---
+++ AzureStorageUtil.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EWSTestConsole
+{
+    public class AzureStorageUtil
+    {
+        public static async Task<string> UploadFile(string fileName, byte[] fileBytes)
+        {
+            return await UploadFile(fileName, "emails", fileBytes);
+        }
+        public static async Task<string> UploadFile(string fileName, string container, byte[] fileBytes)
+        {
+            //get configuration data
+            string url = null;
+            string azureBlobProtocol = ConfigurationManager.AppSettings["abs:Protocol"];
+            string azureBlobAccountName = ConfigurationManager.AppSettings["abs:AccountName"];
+            string azureBlobAccountkey = ConfigurationManager.AppSettings["abs:AccountKey"];
+            container = container.Replace(".", "");
+
+            // Initialize the Azure account information
+            string connString = string.Format("DefaultEndpointsProtocol={0};AccountName={1};AccountKey={2}",
+                azureBlobProtocol, azureBlobAccountName, azureBlobAccountkey);
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(connString);
+
+            // Create the blob client, which provides authenticated access to the Blob service.
+            CloudBlobClient blobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            // Get the container reference...create if it does not exist
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference(container);
+            if (!blobContainer.Exists())
+                await blobContainer.CreateAsync();
+            CloudBlockBlob blob = blobContainer.GetBlockBlobReference(fileName);
+
+            // Set permissions on the container.
+            BlobContainerPermissions containerPermissions = new BlobContainerPermissions();
+            containerPermissions.PublicAccess = BlobContainerPublicAccessType.Off;
+            await blobContainer.SetPermissionsAsync(containerPermissions);
+
+            //upload the file using a memory stream
+            using (MemoryStream stream = new MemoryStream(fileBytes))
+            {
+                stream.Write(fileBytes, 0, fileBytes.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.Flush();
+                blob.UploadFromStream(stream);
+                stream.Close();
+
+                //get the url of the blob
+                url = String.Format("{0}://{1}.blob.core.windows.net/{2}/{3}", azureBlobProtocol,
+                    azureBlobAccountName, container, fileName);
+            }
+
+            return url;
+        }
+        public static async Task<bool> DeleteFile(string fileName, string container)
+        {
+            //get configuration data
+            string azureBlobProtocol = ConfigurationManager.AppSettings["abs:Protocol"];
+            string azureBlobAccountName = ConfigurationManager.AppSettings["abs:AccountName"];
+            string azureBlobAccountkey = ConfigurationManager.AppSettings["abs:AccountKey"];
+            container = container.Replace(".", "");
+
+            // Initialize the Azure account information
+            string connString = string.Format("DefaultEndpointsProtocol={0};AccountName={1};AccountKey={2}",
+                azureBlobProtocol, azureBlobAccountName, azureBlobAccountkey);
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(connString);
+
+            // Create the blob client, which provides authenticated access to the Blob service.
+            CloudBlobClient blobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            // Get the container reference...create if it does not exist
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference(container);
+            if (!blobContainer.Exists())
+                return true;
+
+            try
+            {
+                CloudBlockBlob blob = blobContainer.GetBlockBlobReference(fileName);
+                await blob.DeleteAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EWSTestConsole
+{
+    class Program
+    {
+        private static AutoResetEvent Signal;
+        static void Main(string[] args)
+        {
+            // Establish connection to Exchange with the account
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2013);
+            service.Credentials = new WebCredentials(ConfigurationManager.AppSettings["eml:AccountAddress"], 
+                ConfigurationManager.AppSettings["eml:AccountPassword"]);
+            service.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+            service.AutodiscoverUrl(ConfigurationManager.AppSettings["eml:AccountAddress"], 
+                RedirectionUrlValidationCallback);
+
+            // Setup subscription to listen for new messages
+            var subscription = service.SubscribeToStreamingNotifications(
+                new FolderId[] { WellKnownFolderName.Inbox }, EventType.NewMail);
+            StreamingSubscriptionConnection connection = new StreamingSubscriptionConnection(service, 1);
+            connection.AddSubscription(subscription);
+
+            // Setup delegate event handlers for the subscription connection 
+            connection.OnNotificationEvent += Connection_OnNotificationEvent;
+            connection.OnSubscriptionError += Connection_OnSubscriptionError;
+            connection.OnDisconnect += Connection_OnDisconnect;
+            connection.Open();
+            Console.WriteLine($"Connection established at {DateTime.Now.ToLongTimeString()}.");
+
+            // Wait for the application to exit
+            Signal = new AutoResetEvent(false);
+            Signal.WaitOne();
+        }
+
+        private static void Connection_OnDisconnect(object sender, SubscriptionErrorEventArgs args)
+        {
+            // Cast the sender as a StreamingSubscriptionConnection object            
+            StreamingSubscriptionConnection connection = (StreamingSubscriptionConnection)sender;
+
+            // Reopen the connection
+            if (!connection.IsOpen)
+            {
+                connection.Open();
+                Console.WriteLine($"Connection renewed at {DateTime.Now.ToLongTimeString()}.");
+            }
+        }
+
+        private static void Connection_OnSubscriptionError(object sender, SubscriptionErrorEventArgs args)
+        {
+            throw args.Exception;
+
+            // TODO: put additional logic for handling exceptions
+        }
+
+        private static async void Connection_OnNotificationEvent(object sender, NotificationEventArgs args)
+        {
+            StreamingSubscription subscription = args.Subscription;
+
+            // Loop through all item-related events.  
+            foreach (NotificationEvent notification in args.Events)
+            {
+                EmailMessage email = EmailMessage.Bind(subscription.Service, ((ItemEvent)notification).ItemId);
+                email.Load(new PropertySet(ItemSchema.MimeContent));
+                var mimeContent = email.MimeContent;
+
+                // Generate a random file id for storage and data mapping
+                var id = Guid.NewGuid().ToString();
+                await AzureStorageUtil.UploadFile($"{id}.eml", mimeContent.Content);
+                Console.WriteLine($"Message processed at {DateTime.Now.ToLongTimeString()}.");
+            }
+        }
+
+        static bool RedirectionUrlValidationCallback(String redirectionUrl)
+        {
+            // Perform validation.
+            return (redirectionUrl == "https://autodiscover-s.outlook.com/autodiscover/autodiscover.xml");
+        }
+    }
+}
